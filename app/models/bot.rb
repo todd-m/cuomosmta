2411c6391890af6e7136350b9db8d860ba5f7ce1
Todd <tmartino@mdsol.com>
2017-06-20 23:07:54 -0400

    tweaks probably

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,6 +1,23 @@
 class Bot < ActiveRecord::Base
 
+  def self.find_user(number, words)
+    # find tweets
+    TWITTER_CLIENT.search(words, lang: 'en').take(number).each do |tweet|
+
+      # record twitter user so we don't spam them
+      User.create(name: tweet.user.screen_name, tweet_id: tweet.id.to_s)
+
+      # send a tweet by calling the respond method;
+      # the new tweet is a reply to the saved tweet_id
+      TWITTER_CLIENT.update(Bot.respond(tweet.user.screen_name), in_reply_to_status_id: tweet.id)
+    end
+  end
+
   def self.search_words(words)
     TWITTER_CLIENT.search(words, lang: 'en').first.text
+  end
+
+  def self.respond(name)
+    "@#{name} #{Response.last}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621021823) do
+ActiveRecord::Schema.define(version: 20170621022257) do
 
   create_table "bots", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170621021823) do
 
   create_table "responses", force: :cascade do |t|
     t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
