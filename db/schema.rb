e921bf004d98bcbe65588690c411fa272e43c2db
Todd <tmartino@mdsol.com>
2017-06-20 22:28:02 -0400

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621021258) do
+ActiveRecord::Schema.define(version: 20170621021823) do
 
   create_table "bots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-Response.create(message: "\"[People] in charge\" of the @MTA = @NYGovCuomo #CuomosMTA https://t.co/CYTUWQ3v1l")
+Response.create(message: '"[People] in charge" of the .@MTA = .@NYGovCuomo #CuomosMTA https://t.co/CYTUWQ3v1l')
